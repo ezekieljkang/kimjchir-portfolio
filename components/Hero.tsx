3b656c04374b59f84f8c7796a97af9cb3634f0fb
ezekieljkang <ezekieljkang@gmail.com>
2024-07-30 20:40:43 -0700

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
               I&apos;ve been designing digital products with media and publishing brands since 2021. My passion is to make an impact in people&apos;s everyday lives.
             </p>
           </div>
-          <Image src="/star.SVG" alt="star" width={16} height={20} />
+          <Image src="/star.svg" alt="star" width={16} height={20} />
         </div>
         <div className='md:flex-1 py-[2rem] flex items-center'>
           <div className='relative w-full' style={{paddingBottom: '75%'}}>
