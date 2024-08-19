8c7ad960eb7b805caacf0ac19d1c7c4a276c2444
ezekieljkang <ezekieljkang@gmail.com>
2024-08-18 19:14:28 -0700

    update cs hero and content

--- components/CSHero.tsx
+++ components/CSHero.tsx
@@ -18,31 +18,39 @@ const CSHero: React.FC<HeaderProps> = ({ company, heading, content, timeline, ro
     <div className='px-[3rem]'>
       <NavBar />
       <div>
-      <div>
-        <div className='flex justify-between py-[3.5rem] border-b border-black gap-[3rem]'>
-          <div className='basis-2/3'>
-            <div className='border-r border-black'>
-              <Image src={`${prefix}/${company}`} alt="star" width={120} height={18} className='pb-[14px]' />
-              <h1 className='text-[80px] font-[500] font-clashDisplay'>{heading}</h1>
-              <p className='font-satoshi font-[400] text-[28px]'>{content}</p>
+        <div>
+          <div className='flex justify-between py-[3.5rem] border-b border-black gap-[3rem]'>
+            <div className='basis-2/3'>
+              <div className='border-r border-black'>
+                <Image src={`${prefix}/${company}`} alt="star" width={120} height={18} className='pb-[14px]' />
+                <h1 className='text-[80px] font-[500] font-clashDisplay'>{heading}</h1>
+                <p className='font-satoshi font-[400] text-[28px]'>{content}</p>
+              </div>
+            </div>
+            <div className='basis-1/3 font-normal text-[15px] font-robotoMono'>
+              <div>
+                <p className='pb-[7px] text-customGray'>TIMELINE</p>
+                <p className='pb-[24px]'>{timeline}</p>
+              </div>
+              <div>
+                <p className='pb-[7px] text-customGray'>ROLE</p>
+                <p className='pb-[24px]'>{role}</p>
+              </div>
+              <div>
+                <p className='text-customGray pb-[7px]'>TOOLS</p>
+                <p>{tools}</p>
+              </div>
             </div>
           </div>
-          <div className='basis-1/3 font-normal text-[15px] font-robotoMono'>
-            <div>
-              <p className='pb-[7px] text-customGray'>TIMELINE</p>
-              <p className='pb-[24px]'>{timeline}</p>
-            </div>
-            <div>
-              <p className='pb-[7px] text-customGray'>ROLE</p>
-              <p className='pb-[24px]'>{role}</p>
-            </div>
-            <div>
-              <p className='text-customGray pb-[7px]'>TOOLS</p>
-              <p>{tools}</p>
-            </div>
+          <div>
+            <Image 
+              src={`${prefix}/casestudypic.png`}
+              alt='pictures of redesigned websites'
+              width={100}
+              height={100}
+            />
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
