47d612b76754fcb3055f0a4fedac5da6d53e7e09
ezekieljkang <ezekieljkang@gmail.com>
2024-10-25 23:09:47 -0700

    Update CSContent.tsx

--- components/CSContent.tsx
+++ components/CSContent.tsx
@@ -71,9 +71,9 @@ const CSContent: React.FC<HeaderProps> = ({ problemText, solutionText, resultTex
           <p>ECOMMERCE</p>
         </div>
         <div className='flex items-center gap-[1rem]'>
-          <p>SITE REDESIGN</p>
           <Link href="/pages/PMC/DesignProcess">
-            <button className="w-10 h-10 bg-no-repeat bg-center bg-contain" style={{ backgroundImage: `url(${prefix}/rightarrow.svg)` }}></button>
+            <p className='group-hover:text-gray-400 group-hover:underline'>SITE REDESIGN</p>
+            <button className="w-10 h-10 bg-no-repeat bg-center bg-contain group-hover:bg-gray-400" style={{ backgroundImage: `url(${prefix}/rightarrow.svg)` }}></button>
           </Link>
         </div>
       </div>
