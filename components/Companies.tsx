2bb93ea254b414f07eaff216d504a7050f45ac4e
ezekieljkang <ezekieljkang@gmail.com>
2024-10-25 20:43:37 -0700

    horizontal and vertical center companies.tsx

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -7,7 +7,7 @@ const Companies = () => {
   return (
     <div className="px-[1.5rem] md:px-[3rem] pb-[3.5rem]">
       <div className='border-b border-black md:flex lg:py-[1.5rem]'>
-        <div className='md:border-r md:border-black md:pr-[1rem] lg:py-[1.5rem]'>
+        <div className='flex justify-center items-center md:border-r md:border-black md:pr-[1rem] lg:py-[1.5rem]'>
             <p className='font-robotoMono font-normal text-center'>BRANDS I&apos;VE WORKED WITH</p>
         </div>
         <div className='flex items-center whitespace-nowrap overflow-hidden pb-[1rem] md:pb-0 lg:mx-auto'>
